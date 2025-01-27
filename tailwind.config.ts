5f55f797228d287ae5538effc4b9594a42123e02
eugeny <zhiryakoved@72to.ru>
2025-01-27 20:43:41 +0500

    добавление библиотек. настройка prime и tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+    plugins: [require('tailwindcss-primeui')],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
