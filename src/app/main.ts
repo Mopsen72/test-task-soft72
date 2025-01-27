7615bc984c87152b27c93c6547a20e61049b1569
eugeny <zhiryakoved@72to.ru>
2025-01-27 21:13:14 +0500

    fix started

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,5 +2,7 @@ import {createApp} from 'vue';
 import '../shared/css/style.css';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import {router} from "./router";
 
-createApp(App).use(PrimeVue).mount('#app')
+
+createApp(App).use(router).use(PrimeVue).mount('#app')
